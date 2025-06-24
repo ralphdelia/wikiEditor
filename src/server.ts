26bff2bf188f5abbf3fe642c8d7a8da86018204c
Ralph Delia <Ralphdelia1@gmail.com>
2025-06-24 08:42:21 -0400

    routes cleanup

--- src/server.ts
+++ src/server.ts
@@ -2,28 +2,23 @@ import { Hono } from "hono";
 import { serve } from "bun";
 import { z } from "zod";
 import { zValidator } from "@hono/zod-validator";
-import MessageManager from "./message/manager";
-import MessageProcessor from "./message/processor";
 import { Bus } from "./bus/bus";
 
-const messagesSchema = z.object({
-  messages: z.array(z.any()),
-});
-
 const app = new Hono();
-const manager = new MessageManager();
-const processor = new MessageProcessor(manager);
-void processor.run().catch(console.error);
-
-app.get("/test", (c) => c.text("Test route is working!"));
 
 app.post(
   "/messages",
-  zValidator("json", messagesSchema, (result, c) => {
-    if (!result.success) {
-      return c.json({ error: result.error.format() }, 400);
-    }
-  }),
+  zValidator(
+    "json",
+    z.object({
+      messages: z.array(z.any()),
+    }),
+    (result, c) => {
+      if (!result.success) {
+        return c.json({ error: result.error.format() }, 400);
+      }
+    },
+  ),
   (c) => {
     const { messages } = c.req.valid("json");
     messages.forEach((message) => {
